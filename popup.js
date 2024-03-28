daa3dc69dda0fc71cb245d13e44bf8a2a3e28b73
GNakhtigal <GNakhtigal@synergy.ru>
2024-03-28 11:02:54 +0500

    feature/ | create styled speed range

---
+++ popup.js
@@ -0,0 +1,10 @@
+console.log('popup.js');
+// console.log(
+//   chrome.tabs.getAllInWindow(
+//     1 // optional integer
+//   )
+// );
+// document.getElementById('visibleBtn').addEventListener('click', () => {
+//   console.log('click');
+// });
+// console.log(document.getElementById('visibleBtn'));
---
+++ script.js
@@ -0,0 +1,41 @@
+const rangeElem = `
+	<input 
+		type="range" 
+		name="speed" 
+		id="speedId" 
+		value="1"
+		min="0.1" 
+		max="10" 
+		step="0.1" 
+		style="
+			width: 300px;
+		"
+	/>
+`;
+const wrap = document.createElement('div');
+wrap.style =
+  'position: fixed; z-index: 9999; top: 0; left: 0; padding: 10px; background-color: tomato;';
+wrap.innerHTML = rangeElem;
+
+document.body.prepend(wrap);
+const rangeHTML = document.getElementById('speedId');
+
+let speedValue = rangeHTML.value;
+const speedValueHtml = document.createElement('p');
+speedValueHtml.style = 'color: white; font-size: 20px;';
+speedValueHtml.innerHTML = `Speed: ${speedValue}`;
+
+wrap.append(speedValueHtml);
+
+rangeHTML.addEventListener('input', (e) => {
+  speedValue = e.target.value;
+  speedValueHtml.innerHTML = `Speed: ${speedValue}`;
+  console.log(speedValue);
+  document.getElementsByClassName('video-stream html5-main-video')[0].playbackRate = speedValue;
+});
+
+// console.log(chrome.tabs.create({ url: 'https://www.youtube.com/' }));
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.tabs.create({ url: 'https://www.youtube.com/' });
+// });
+console.log(1111);
