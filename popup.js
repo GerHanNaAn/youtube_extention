698c5d58cecd692c206d3a689f9ac4ebd0ea4934
GNakhtigal <GNakhtigal@synergy.ru>
2024-03-29 11:34:44 +0500

    feature/ | add chrome storage async function

--- popup.js
+++ popup.js
@@ -1,18 +1,27 @@
-console.log('popup.js');
-// console.log(
-//   chrome.tabs.getAllInWindow(
-//     1 // optional integer
-//   )
-// );
-// document.getElementById('visibleBtn').addEventListener('click', () => {
-//   console.log('click');
-// });
-console.log(document.getElementById('visibleBtn'));
-console.log(document);
-
 const btn = document.getElementById('visibleBtn');
-
-btn.addEventListener('click', () => {
-	console.log('click');
-	chrome.tabs.create({ url: 'https://www.youtube.com/' });
+btn.addEventListener('click', async () => {
+  let res = '2222';
+  await chrome.storage.local.set({ speedExtVisible: false });
+  await chrome.storage.local.get('speedExtVisible').then((e) => {
+    res = e;
+    console.log(res);
+  });
 });
+// setTimeout(() => {
+//   console.log(сhrome.storage);
+// }, 1000);
+// console.log(сhrome.storage.local);
+// console.log(сhrome.storage.local);
+// let extState = сhrome.storage.sync.get({'speedExtVisible'});
+// console.log(extState);
+// btn.addEventListener('click', () => {
+//   if (extState.speedExtVisible) {
+//     chrome.storage.local.set({ speedExtVisible: false });
+//     btn.innerText = 'Show';
+//     return;
+//   } else {
+//     chrome.storage.local.set({ speedExtVisible: true });
+//     btn.innerText = 'Hide';
+//     return;
+//   }
+// });
